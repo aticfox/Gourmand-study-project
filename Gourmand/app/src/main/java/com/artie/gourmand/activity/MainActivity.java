01017e20e694e70e73eac3150a3be407ba0e7bc1
Kanticha <aticfox@gmail.com>
2017-05-23 22:26:13 +0700

    removed wrong spacing at onCreateOptionsMenu method in MainActivity

--- Gourmand/app/src/main/java/com/artie/gourmand/activity/MainActivity.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/MainActivity.java
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        
         return true;
-
     }
 
     @Override
