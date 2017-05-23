5bebba1cddf720e7481bfcf86d45c28aec2f99e7
Kanticha <aticfox@gmail.com>
2017-05-23 18:44:00 +0700

    linked action to feed screen

--- Gourmand/app/src/main/java/com/artie/gourmand/activity/MainActivity.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.artie.gourmand.R;
+import com.artie.gourmand.fragment.FeedFragment;
 import com.artie.gourmand.fragment.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         if(savedInstanceState == null) {
             ProfileFragment profileFragment = ProfileFragment.newInstance();
+            FeedFragment feedFragment = FeedFragment.newInstance();
             getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer,
+                            feedFragment,
+                            "FeedFragment")
                     .add(R.id.contentContainer,
                             profileFragment,
                             "ProfileFragment")
@@ -46,10 +51,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        ProfileFragment profileFragment = (ProfileFragment) getSupportFragmentManager()
+                .findFragmentByTag("ProfileFragment");
+        FeedFragment feedFragment = (FeedFragment) getSupportFragmentManager()
+                .findFragmentByTag("FeedFragment");
+
         if(item.getItemId() == R.id.menu_profile) {
-            ProfileFragment profileFragment = (ProfileFragment) getSupportFragmentManager().findFragmentByTag("ProfileFragment");
             getSupportFragmentManager().beginTransaction()
+                    .detach(feedFragment)
                     .attach(profileFragment)
+                    .commit();
+        } else if (item.getItemId() == R.id.menu_feed) {
+            getSupportFragmentManager().beginTransaction()
+                    .detach(profileFragment)
+                    .attach(feedFragment)
                     .commit();
         }
         return super.onOptionsItemSelected(item);
