761d68b23ab72599115791c6d4a5f33beb657db5
Kanticha <aticfox@gmail.com>
2017-05-25 20:21:26 +0700

    implemented photo grid

--- Gourmand/app/src/main/java/com/artie/gourmand/fragment/ProfileFragment.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/fragment/ProfileFragment.java
@@ -3,17 +3,24 @@ package com.artie.gourmand.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.artie.gourmand.R;
+import com.artie.gourmand.adapter.GridSquarePhotoAdapter;
 
 /**
  * Created by ANFIELD on 23/5/2560.
  */
 
 public class ProfileFragment extends Fragment {
+
+    static final int PHOTO_COLUMN = 3;
+
+    RecyclerView mRecyclerView;
 
     public static ProfileFragment newInstance() {
         Bundle args = new Bundle();
@@ -35,7 +42,9 @@ public class ProfileFragment extends Fragment {
     }
 
     private void initInstances(View rootView) {
-
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mRecyclerView.setAdapter(new GridSquarePhotoAdapter(PHOTO_COLUMN));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), PHOTO_COLUMN));
     }
 
 }
