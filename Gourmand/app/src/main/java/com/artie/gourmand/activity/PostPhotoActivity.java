2b841c63419c39ce0ebecc955c3bf18cd54fc7da
Kanticha <aticfox@gmail.com>
2017-05-27 15:56:35 +0700

    implemented post photo to feed screen

--- Gourmand/app/src/main/java/com/artie/gourmand/activity/PostPhotoActivity.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/PostPhotoActivity.java
@@ -2,8 +2,10 @@ package com.artie.gourmand.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.artie.gourmand.R;
 
@@ -18,6 +20,27 @@ public class PostPhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_photo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_post_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_post:
+                Intent intent = MainActivity.getStartIntent(PostPhotoActivity.this);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
