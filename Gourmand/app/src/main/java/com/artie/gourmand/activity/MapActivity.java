6c26dbd112ad2260066362531991fc4695b21ce3
Kanticha <aticfox@gmail.com>
2017-06-04 18:09:22 +0700

    connected profile screen to member map screen

---
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/MapActivity.java
@@ -0,0 +1,23 @@
+package com.artie.gourmand.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.artie.gourmand.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    public static Intent getStartIntent(Context context) {
+        Intent intent = new Intent(context, MapActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+    
+}
