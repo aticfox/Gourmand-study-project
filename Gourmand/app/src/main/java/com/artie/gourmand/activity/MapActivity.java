dd3be1a1076cb4fcd6dd0fad3d2011ec4d303a60
Kanticha <aticfox@gmail.com>
2017-06-06 22:59:39 +0700

    implemented map screen

--- Gourmand/app/src/main/java/com/artie/gourmand/activity/MapActivity.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/MapActivity.java
@@ -6,8 +6,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.artie.gourmand.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
 
     public static Intent getStartIntent(Context context) {
         Intent intent = new Intent(context, MapActivity.class);
@@ -18,6 +26,16 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
