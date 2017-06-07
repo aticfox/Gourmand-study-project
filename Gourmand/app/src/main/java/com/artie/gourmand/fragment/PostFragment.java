5dbc7aabd34e5a3711b06e2547ee0996ebb6bf2a
Kanticha <aticfox@gmail.com>
2017-06-08 00:59:05 +0700

    connected post screen to map screen

--- Gourmand/app/src/main/java/com/artie/gourmand/fragment/PostFragment.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/fragment/PostFragment.java
@@ -1,19 +1,24 @@
 package com.artie.gourmand.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.artie.gourmand.R;
+import com.artie.gourmand.activity.MapActivity;
 
 /**
  * Created by ANFIELD on 3/6/2560.
  */
 
 public class PostFragment extends Fragment {
+
+    TextView mTextLocationName;
 
     public static FeedFragment newInstance() {
         Bundle args = new Bundle();
@@ -35,7 +40,14 @@ public class PostFragment extends Fragment {
     }
 
     private void initInstances(View rootView) {
-
+        mTextLocationName = (TextView) rootView.findViewById(R.id.text_location_name);
+        mTextLocationName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MapActivity.getStartIntent(getContext());
+                startActivity(intent);
+            }
+        });
     }
 
 }
