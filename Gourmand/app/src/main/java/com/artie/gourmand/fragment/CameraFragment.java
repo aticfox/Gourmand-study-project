963dfa472538b68b18cee0f30ed7ba6fbded47ce
Kanticha <aticfox@gmail.com>
2017-06-26 01:25:47 +0700

    implemented take photo from camera to create post screen

--- Gourmand/app/src/main/java/com/artie/gourmand/fragment/CameraFragment.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/fragment/CameraFragment.java
@@ -1,9 +1,19 @@
 package com.artie.gourmand.fragment;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +21,10 @@ import android.widget.Button;
 
 import com.artie.gourmand.R;
 import com.artie.gourmand.activity.CreatePostActivity;
+import com.flurgle.camerakit.CameraListener;
 import com.flurgle.camerakit.CameraView;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by ANFIELD on 26/5/2560.
@@ -21,6 +34,8 @@ public class CameraFragment extends Fragment {
 
     Button mButtonTakePhoto;
     CameraView mCameraView;
+
+    private Uri mImageURI;
 
     public static CameraFragment newInstance() {
         Bundle args = new Bundle();
@@ -36,6 +51,17 @@ public class CameraFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         init(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Nullable
@@ -54,22 +80,30 @@ public class CameraFragment extends Fragment {
 
     private void initInstances(View rootView) {
         mCameraView = (CameraView) rootView.findViewById(R.id.camera);
+        mCameraView.setCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] jpeg) {
+                super.onPictureTaken(jpeg);
+
+                Bitmap result = BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length);
+                mImageURI = getImageUri(getContext(), result);
+
+                Intent intent = CreatePostActivity.getStartIntent(getContext(), mImageURI);
+                startActivity(intent);
+            }
+        });
 
         mButtonTakePhoto = (Button) rootView.findViewById(R.id.button_take_photo);
         mButtonTakePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-
                 switch (v.getId()) {
                     case R.id.button_take_photo :
-                        intent = CreatePostActivity.getStartIntent(getContext(), null);
+                        mCameraView.captureImage();
                         break;
                     default:
                         return;
                 }
-
-                startActivity(intent);
             }
         });
     }
@@ -84,6 +118,13 @@ public class CameraFragment extends Fragment {
     public void onPause() {
         super.onPause();
         mCameraView.stop();
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 
 }
