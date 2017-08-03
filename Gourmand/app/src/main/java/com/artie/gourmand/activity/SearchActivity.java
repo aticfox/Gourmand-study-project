7cd4c7529478b3ac522103c3a791d9ffaf983801
Kanticha <aticfox@gmail.com>
2017-08-03 23:00:43 +0700

    implemented search filter

--- Gourmand/app/src/main/java/com/artie/gourmand/activity/SearchActivity.java
+++ Gourmand/app/src/main/java/com/artie/gourmand/activity/SearchActivity.java
@@ -7,12 +7,19 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.artie.gourmand.R;
 import com.artie.gourmand.adapter.SearchAdapter;
 import com.artie.gourmand.dao.MemberItemCollectionDao;
+import com.artie.gourmand.dao.MemberItemDao;
 import com.artie.gourmand.manager.HttpManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,7 +28,9 @@ import retrofit2.Response;
 public class SearchActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerViewFriend;
+    EditText mEditTextSearch;
 
+    private MemberItemCollectionDao mDao;
     private SearchAdapter mSearchAdapter;
 
     public static Intent getStartIntent(Context context) {
@@ -45,9 +54,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<MemberItemCollectionDao> call, Response<MemberItemCollectionDao> response) {
                 if (response.isSuccessful()) {
-                    MemberItemCollectionDao dao = response.body();
-                    mSearchAdapter.setDao(dao);
-                    mSearchAdapter.notifyDataSetChanged();
+                    mDao = response.body();
                 }
             }
 
@@ -62,11 +69,50 @@ public class SearchActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Search Friend");
 
+        mEditTextSearch = (EditText) findViewById(R.id.edit_text_search);
+        mEditTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                MemberItemCollectionDao dao;
+                if (s.toString().isEmpty()) {
+                    dao = new MemberItemCollectionDao(new ArrayList<MemberItemDao>());
+                } else {
+                    dao = filter(mDao, s.toString());
+                }
+                mSearchAdapter.setDao(dao);
+                mSearchAdapter.notifyDataSetChanged();
+            }
+        });
         mSearchAdapter = new SearchAdapter(SearchActivity.this, new MemberItemCollectionDao());
 
         mRecyclerViewFriend = (RecyclerView) findViewById(R.id.recycler_view_select_friend);
         mRecyclerViewFriend.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         mRecyclerViewFriend.setAdapter(mSearchAdapter);
+    }
+
+    private MemberItemCollectionDao filter(MemberItemCollectionDao mDao, String keyword) {
+        List<MemberItemDao> members = new ArrayList<>();
+        for (int i=0; i<mDao.getMembers().size(); i++) {
+            MemberItemDao member = mDao.getMembers().get(i);
+            if (isMemberNameSameKeyword(member.getName(), keyword)) {
+                members.add(member);
+            }
+        }
+        return new MemberItemCollectionDao(members);
+    }
+
+    private boolean isMemberNameSameKeyword(String name, String keyword) {
+        return name.toLowerCase().contains(keyword.toLowerCase());
     }
 
 }
